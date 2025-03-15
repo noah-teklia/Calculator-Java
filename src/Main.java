c2e65d981458761b17b8ceab78fee86dcbcabc64
Noah Teklia <nteklia@gmail.com>
2025-03-15 08:55:41 +0300

    Subtraction method added

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,17 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-        double x=addition(5, 7);
-        System.out.println("x: "+ x);
+        double x = addition(5, 7);
+        System.out.println("x: " + x);
+        double Subtraction = subtract(20, 15);
+        System.out.print("Subtraction: " + Subtraction);
     }
+
     public static double addition(double a, double b) {
         return a + b;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
     }
 }
